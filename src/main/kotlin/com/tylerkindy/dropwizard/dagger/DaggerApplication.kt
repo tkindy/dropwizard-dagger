a46bbb087ad43d82e51511da4fbc80db8797e773
Tyler Kindy <tylerkindy@gmail.com>
2018-08-26 15:19:01 -0400

    Add annotation

--- src/main/kotlin/com/tylerkindy/dropwizard/dagger/DaggerApplication.kt
+++ src/main/kotlin/com/tylerkindy/dropwizard/dagger/DaggerApplication.kt
@@ -3,6 +3,7 @@ package com.tylerkindy.dropwizard.dagger
 import io.dropwizard.Application
 import io.dropwizard.Configuration
 import io.dropwizard.setup.Environment
+import javax.annotation.OverridingMethodsMustInvokeSuper
 import javax.inject.Inject
 
 abstract class DaggerApplication<C : Configuration> : Application<C>() {
@@ -15,6 +16,7 @@ abstract class DaggerApplication<C : Configuration> : Application<C>() {
     @JvmSuppressWildcards
     internal lateinit var healthChecks: Set<NamedHealthCheck>
 
+    @OverridingMethodsMustInvokeSuper
     override fun run(configuration: C, environment: Environment) {
         @Suppress("UNCHECKED_CAST")
         (applicationInjector(configuration) as DropwizardInjector<DaggerApplication<*>>).inject(this)
